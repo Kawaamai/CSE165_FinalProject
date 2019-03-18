df003dee1ec4206e8e431396626594538dbecdd1
Alan D Mai <admai@UCSD.EDU>
2019-03-17 18:11:12 -0700

    initial cutting framework

--- Assets/Cuttable.cs
+++ Assets/Cuttable.cs
@@ -6,6 +6,9 @@ public class Cuttable : MonoBehaviour {
 
 	public GameObject CuttableObjectPrefab;
 
+    private enum HitDirection {  None, Top, Bottom, Forward, Back, Left, Right }
+    private enum CubeAxis { None, X, Y, Z }
+
 	// Use this for initialization
 	void Start () {
 		
@@ -30,16 +33,103 @@ public class Cuttable : MonoBehaviour {
 			Debug.Log(collision.relativeVelocity);
 			Debug.Log(collision.contacts.Length); // usually 1, sometimes 2
 			ContactPoint contact = collision.contacts[0];
-			Debug.Log(contact.point - transform.position);
+            //Debug.Log(contact.point - transform.position);
+            Debug.Log(ReturnDirection(contact.point));
 		}
     }
 
-	private void Split()
-	{
-		// TODO:
-		// Create two other CuttableObjectPrefabs
-		// position and size correctly
-		// remove this one
-		// maybe add a force so that the two smaller objects split
-	}
+    private void GetInnerCollisionNormal(Vector3 contactPoint, out Vector3 innerNormal, out RaycastHit innerRayHit)
+    {
+        // this should be collision contact point
+        Vector3 direction = (contactPoint - transform.position).normalized;
+        Ray innerRay = new Ray(transform.position, direction);
+        innerNormal = Vector3.zero;
+
+        if (Physics.Raycast(innerRay, out innerRayHit))
+        {
+            if (innerRayHit.collider != null)
+            {
+                innerNormal = innerRayHit.normal;
+                innerNormal = innerRayHit.transform.TransformDirection(innerNormal);
+            }
+        }
+    }
+
+    private HitDirection ReturnDirection(Vector3 contactPoint)
+    {
+        HitDirection hitDirection = HitDirection.None;
+        Vector3 innerNormal;
+        RaycastHit innerRayHit;
+        GetInnerCollisionNormal(contactPoint, out innerNormal, out innerRayHit);
+
+        if (innerNormal == innerRayHit.transform.up)
+            hitDirection = HitDirection.Top;
+        if (innerNormal == -innerRayHit.transform.up)
+            hitDirection = HitDirection.Bottom;
+        if (innerNormal == innerRayHit.transform.forward)
+            hitDirection = HitDirection.Forward;
+        if (innerNormal == -innerRayHit.transform.forward)
+            hitDirection = HitDirection.Back;
+        if (innerNormal == innerRayHit.transform.right)
+            hitDirection = HitDirection.Right;
+        if (innerNormal == -innerRayHit.transform.right)
+            hitDirection = HitDirection.Left;
+
+        return hitDirection;
+    }
+
+    private void GetCutAxis(GameObject collidingObject, Vector3 contactPoint, HitDirection hitDir, out CubeAxis axis, out float magnitude)
+    {
+        axis = CubeAxis.None;
+        magnitude = 0f;
+        if (hitDir == HitDirection.None)
+            return;
+
+        Vector3 outerPlaneNormal;
+        RaycastHit raycastHit;
+        GetInnerCollisionNormal(contactPoint, out outerPlaneNormal, out raycastHit);
+        outerPlaneNormal = -outerPlaneNormal;
+
+        Quaternion rot = collidingObject.transform.rotation;
+        Vector3 dir = rot * Vector3.up; // TODO: figure out if this is up or forward
+        Vector3 proj = Vector3.ProjectOnPlane(dir, outerPlaneNormal);
+
+        Quaternion invRot = Quaternion.Inverse(transform.rotation);
+        proj = invRot * proj;
+        proj.Normalize();
+
+        if (hitDir == HitDirection.Top || hitDir == HitDirection.Bottom)
+        {
+            // y plane; ignore y component
+            if (proj.x < proj.z)
+            {
+                axis = CubeAxis.X;
+                magnitude = proj.x;
+            }
+            axis = CubeAxis.Z;
+            magnitude = proj.z;
+        }
+        else if (hitDir == HitDirection.Forward || hitDir == HitDirection.Back)
+        {
+            // z plane; ignore z component
+            if (proj.x < proj.y)
+            {
+                axis = CubeAxis.X;
+                magnitude = proj.x;
+            }
+            axis = CubeAxis.Y;
+            magnitude = proj.y;
+        }
+        else // hitDir == HitDirection.Left || HitDirection.Right
+        {
+            // x plane; ignore x component
+            if (proj.z < proj.y)
+            {
+                axis = CubeAxis.Z;
+                magnitude = proj.x;
+            }
+            axis = CubeAxis.Y;
+            magnitude = proj.y;
+        }
+    }
 }
