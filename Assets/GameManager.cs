2571b43490f329338b9af4a483c8410ab1154f9a
stfnolaf <soloughl@ucsd.edu>
2019-03-21 10:36:48 -0700

    fixed respawning turrets

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -870,11 +870,12 @@ public class GameManager : MonoBehaviour {
 
 	public void Reset()
 	{
-		GameObject[] turrets = GameObject.FindGameObjectsWithTag("Turret");
+		GameObject[] turrets = TurretBehavior.deactivatedTurrets;
 		for(int i = 0; i < turrets.Length; i++)
 		{
-			turrets[i].transform.parent.gameObject.SetActive(true);
+			turrets[i].SetActive(true);
 		}
+		TurretBehavior.deactivatedArrayHead = 0;
 		ai.GetComponent<AIBehavior>().Reset();
 	}
 
